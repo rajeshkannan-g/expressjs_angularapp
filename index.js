768f933f9b4dd8eb8791a8a1878072f8d006a221
Rajeshkannan Gurusamy <rajesh.kannan@servicenow.com>
2018-04-05 16:44:33 +0530

    Update port number and log message.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 
 app.use('/', express.static('public'))
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000, () => console.log('ExpressApp1 Listening on port 3000!'));
+app.listen(server_port, server_ip_address, () => console.log('ExpressApp1 Listening on ' + server_ip_address + ':' +  server_port  + '!'));
